d5f0d0fae6dea89c9a627aab766050a6083398a3
grevz1998 <grevz1998@gmail.com>
2024-02-25 12:43:38 +0300

    Добавил статус выполнения

--- Workspace_cleaner.Designer.cs
+++ Workspace_cleaner.Designer.cs
@@ -34,7 +34,17 @@ namespace Workspace_cleaner
             this.checkBox_logs_posserver = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
             this.checkBox_date_rms = new System.Windows.Forms.CheckBox();
-            this.checkBox_dism = new System.Windows.Forms.CheckBox();
+            this.checkBox_loading = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             this.SuspendLayout();
             // 
             // checkBox_temporary_files
@@ -87,22 +97,67 @@ namespace Workspace_cleaner
             this.checkBox_date_rms.Text = "Очистить данные RMS клиентов";
             this.checkBox_date_rms.UseVisualStyleBackColor = true;
             // 
-            // checkBox_dism
+            // checkBox_loading
             // 
-            this.checkBox_dism.AutoSize = true;
-            this.checkBox_dism.Location = new System.Drawing.Point(13, 109);
-            this.checkBox_dism.Name = "checkBox_dism";
-            this.checkBox_dism.Size = new System.Drawing.Size(94, 17);
-            this.checkBox_dism.TabIndex = 5;
-            this.checkBox_dism.Text = "Очистка Dism";
-            this.checkBox_dism.UseVisualStyleBackColor = true;
+            this.checkBox_loading.AutoSize = true;
+            this.checkBox_loading.Location = new System.Drawing.Point(13, 109);
+            this.checkBox_loading.Name = "checkBox_loading";
+            this.checkBox_loading.Size = new System.Drawing.Size(122, 17);
+            this.checkBox_loading.TabIndex = 5;
+            this.checkBox_loading.Text = "Очистить загрузки";
+            this.checkBox_loading.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(266, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 18);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(266, 36);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 18);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(266, 60);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(20, 18);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(266, 84);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(20, 18);
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(266, 108);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(20, 18);
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
             // 
             // Workspace_cleaner
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(277, 196);
-            this.Controls.Add(this.checkBox_dism);
+            this.ClientSize = new System.Drawing.Size(298, 196);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checkBox_loading);
             this.Controls.Add(this.checkBox_date_rms);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox_logs_posserver);
@@ -113,6 +168,11 @@ namespace Workspace_cleaner
             this.MinimizeBox = false;
             this.Name = "Workspace_cleaner";
             this.Text = "Workspace cleaner";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,6 +185,11 @@ namespace Workspace_cleaner
         private System.Windows.Forms.CheckBox checkBox_logs_posserver;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckBox checkBox_date_rms;
-        private System.Windows.Forms.CheckBox checkBox_dism;
+        private System.Windows.Forms.CheckBox checkBox_loading;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
     }
 }
--- Workspace_cleaner.cs
+++ Workspace_cleaner.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.IO;
+using System.Drawing;
 
 namespace Workspace_cleaner
 {
@@ -19,51 +13,56 @@ namespace Workspace_cleaner
             InitializeComponent();
         }
 
-        //TODO Класс для процесса
-        //TODO Dism.exe /Online /Cleanup-Image /AnalyzeComponentStore
-        //TODO Dism.exe /Online /Cleanup-Image /StartComponentCleanup
+        //TODO Оптимизация кода - убрать повторения, перенести часть программы в мейн
         private void button1_Click(object sender, EventArgs e)
         {
 
+            //Проверяем какие функции включены
             if (checkBox_temporary_files.Checked == true)
             {
-                Temporary();
+                Change_image(pictureBox1, false);
+                Temporary(pictureBox1);
             }
             if (checkBox_logs_30days.Checked == true)
             {
-                Logs_iikoFront();
+                Change_image(pictureBox2, false);
+                int day_delete = 30;
+                Logs_iikoFront(day_delete,pictureBox2);
             }
             if (checkBox_logs_posserver.Checked == true)
 
             {
+                Change_image(pictureBox3, false);
                 int day_delete = 10;
                 string path_first = "C:\\Windows\\ServiceProfiles\\iikoCardPOS\\AppData\\Roaming\\iiko\\iikoCard5\\Logs";
                 string path_second = "C:\\Users\\iikoCard5POS\\AppData\\Roaming\\iiko\\iikoCard5\\Logs";
                 if (Directory.Exists(path_first) == true)
                 {
-                    Logs_Pos_2(path_first, day_delete);
+                    Logs_Pos_2(path_first, day_delete,pictureBox3);
                 }
                 else
                 {
                     if (Directory.Exists(path_second) == true)
                     {
-                        Logs_Pos(path_second, day_delete);
+                        Logs_Pos(path_second, day_delete, pictureBox3);
                     }
                     else
                     {
                         MessageBox.Show("Нет POS сервера на компьютере");
                     }
                 }
-                
+
 
             }
+            // Проверяем наличие папки RMS
             if (checkBox_date_rms.Checked == true)
             {
+                Change_image(pictureBox4, false);
                 string path_RMS = "C:\\Users\\User\\AppData\\Roaming\\iiko\\RMS";
 
                 if (Directory.Exists(path_RMS) == true)
                 {
-                    Delete_date_RMS(path_RMS);
+                    Delete_date_RMS(path_RMS, pictureBox4);
                 }
                 else
                 {
@@ -71,11 +70,18 @@ namespace Workspace_cleaner
                 }
 
             }
+            if (checkBox_loading.Checked == true)
+            {
+                Change_image(pictureBox5, false);
+                Loading_deleted(pictureBox5);
+            }
 
         }
         // Очистка временных файлов
-        public static void Temporary()
+        public static void Temporary(object pic_box_num)
         {
+            var pic_box = pic_box_num as PictureBox;
+
             try
             {
                 ProcessStartInfo ppt = new ProcessStartInfo
@@ -87,37 +93,43 @@ namespace Workspace_cleaner
 
                 Process procCommand = Process.Start(ppt);
                 procCommand.EnableRaisingEvents = true;
-                procCommand.Exited += (sender, e) => { MessageBox.Show("Очистка временных файлов завершена"); };
+                procCommand.Exited += (sender, e) =>
+                {
+                    Change_image(pic_box, true); ;
+                };
+
+
             } //Контроль выполнения процесса
 
             catch { }
 
-
         }
         //Очистка файлов iikoFront которым более 30 дней
-        public static void Logs_iikoFront()
+        public static void Logs_iikoFront(int day, object pic_box_num)
         {
+            var pic_box = pic_box_num as PictureBox;
             try
             {
                 ProcessStartInfo ppt = new ProcessStartInfo
                 {
                     FileName = "cmd.exe",
-                    Arguments = "/c ForFiles /p \"%userprofile%\\AppData\\Roaming\\iiko\\CashServer\\Logs\" /s /c \"cmd /c del @file /f /q\" /d -30",
+                    Arguments = "/c ForFiles /p \"%userprofile%\\AppData\\Roaming\\iiko\\CashServer\\Logs\" /s /c \"cmd /c del @file /f /q\" /d -" + day,
                     WindowStyle = ProcessWindowStyle.Hidden
                 };
 
                 Process procCommand = Process.Start(ppt);
                 procCommand.EnableRaisingEvents = true;
-                procCommand.Exited += (sender, e) => { MessageBox.Show("Очистка логов iikoFront завершена"); };
+                procCommand.Exited += (sender, e) => { Change_image(pic_box, true); };
             }
 
             catch { }
 
 
         }
-        //Очистка логов POS сервера
-        public static void Logs_Pos(string Address, int day)
+        //Очистка логов POS сервера, если путь 1
+        public static void Logs_Pos(string Address, int day,object pic_box_num)
         {
+            var pic_box = pic_box_num as PictureBox;
 
             try
             {
@@ -130,15 +142,17 @@ namespace Workspace_cleaner
 
                 Process procCommand = Process.Start(ppt);
                 procCommand.EnableRaisingEvents = true;
-                procCommand.Exited += (sender, e) => { MessageBox.Show("Очистка логов POS завершена"); };
+                procCommand.Exited += (sender, e) => { Change_image(pic_box, true); };
 
 
             }
 
             catch { };
         }
-        public static void Logs_Pos_2(string Address, int day)
+        //Очистка логов POS сервера, если путь 2
+        public static void Logs_Pos_2(string Address, int day, object pic_box_num)
         {
+            var pic_box = pic_box_num as PictureBox;
 
             try
             {
@@ -153,7 +167,7 @@ namespace Workspace_cleaner
 
                 Process procCommand = Process.Start(ppt);
                 procCommand.EnableRaisingEvents = true;
-                procCommand.Exited += (sender, e) => { MessageBox.Show("Очистка логов POS завершена"); };
+                procCommand.Exited += (sender, e) => { Change_image(pic_box, true); };
 
 
             }
@@ -162,25 +176,25 @@ namespace Workspace_cleaner
         }
 
         //Очистка данные об RMS клиентов 
-        public static void Delete_date_RMS(string Addres)
+        public static void Delete_date_RMS(string Addres, object pic_box_num)
         {
-            
+            var pic_box = pic_box_num as PictureBox;
+
             try
             {
                 ProcessStartInfo ppt = new ProcessStartInfo
                 {
                     FileName = "cmd.exe",
-                    //cd C:\Users\User\AppData\Roaming\iiko & rd /s /q RMS
                     Arguments = "/c cd C:\\Users\\User\\AppData\\Roaming\\iiko & rd /s /q RMS",
-                    WindowStyle = ProcessWindowStyle.Normal
-                    
+                    WindowStyle = ProcessWindowStyle.Hidden
+
 
                 };
 
                 Process procCommand = Process.Start(ppt);
 
                 procCommand.EnableRaisingEvents = true;
-                procCommand.Exited += (sender, e) => { MessageBox.Show("Очистка данных RMS завершена"); };
+                procCommand.Exited += (sender, e) => { Change_image(pic_box, true); };
 
 
             }
@@ -191,5 +205,47 @@ namespace Workspace_cleaner
 
 
         }
+        public static void Loading_deleted(object pic_box_num)
+        {
+            var pic_box = pic_box_num as PictureBox;
+
+            try
+            {
+                ProcessStartInfo ppt = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/c cd %userprofile% & rd /s /q Downloads",
+                    WindowStyle = ProcessWindowStyle.Normal
+
+
+                };
+
+                Process procCommand = Process.Start(ppt);
+
+                procCommand.EnableRaisingEvents = true;
+                procCommand.Exited += (sender, e) => { Change_image(pic_box, true); };
+
+
+            }
+
+            catch { };
+        }
+
+        public static void Change_image(object img_box, bool ready)
+        {
+            var img_box_ = img_box as PictureBox;
+            img_box_.SizeMode = PictureBoxSizeMode.StretchImage;
+            if (ready == true)
+            {
+                img_box_.Image = Image.FromFile(@"img\correct.png");
+            }
+            else 
+            {
+                img_box_.Image = Image.FromFile(@"img\cross.png");
+            }
+            
+            
+        }
+
     }
 }
