ea9dc03a67230c66da6da4d25db6ebc453a13a58
grevz1998 <grevz1998@gmail.com>
2024-03-01 00:00:48 +0300

    Реализовал 1) Удаление файлов через DirectoryInfo 2) Добавил корректный лог

--- WorkspaceCleaner.cs
+++ WorkspaceCleaner.cs
@@ -3,11 +3,16 @@ using System.Windows.Forms;
 using System.IO;
 using System.Drawing;
 using System.Collections.Generic;
+using System.Threading; //Удалить нужно для теста изменения цвета
+
+//TODO Реализовать изменение цвета кнопки, сейчас только зеленый горит
+//TODO Правила написания кода!!!
 
 namespace WorkspaceCleaner
 {
     public partial class WorkspaceCleaner : Form
     {
+        
         public WorkspaceCleaner()
         {
             InitializeComponent();
@@ -19,20 +24,23 @@ namespace WorkspaceCleaner
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //progressBar1.Value += 1;
 
             //Проверяем какие функции включены
             if (checkBox_temporary_files.Checked) //Нужно удалить временные файлы?
             {
+                //ChangeStatus(label1, Color.Green);
                 TemporaryFilesDel();
-                ProgramFilesDel();
+                //ProgramFilesDel();
             }
             if (checkBox_logs_30days.Checked) //Нужно удалить логи iikoFront?
             {
                 LogsFilesDel();
             }
-            if (checkBox_logs_posserver.Checked & PathForPos(out var path)) //Нужно удалить логи POS сервера
+            if (checkBox_logs_posserver.Checked) //Нужно удалить логи POS сервера
             {
-                DeleteFiles(label3, FilesNames(path), "iikoPosLogs", -10);
+                PathForPos(out var path);
+                DeletedFromInfoFiles(path, "iikoPosLogs", label3, -10);
             }
 
 
@@ -46,28 +54,22 @@ namespace WorkspaceCleaner
             }
 
         }
-
         // Меняем статус работы
-        private void ChangeStatus(object label_box, bool ready)
+        private void ChangeStatus(Label label_box, Color color)
         {
-
-            var label_box_ = label_box as Label;
-            label_box_.Text = "  ";
-            if (ready)
-            {
-                label_box_.BackColor = Color.Green;
-            }
-            else
-            {
-                label_box_.BackColor = Color.Red;
-            }
+            label_box.Text = "  ";
+            label_box.BackColor = color;        
         }
-        private void DeleteFiles(object pic_box, IEnumerable<string> folders_full, string name, int DayAgo)
+        private void DeleteFiles(Label LabelBox, IEnumerable<string> folders_full, string name, int DayAgo)
         {
-            ChangeStatus(pic_box, false);
+            ChangeStatus(LabelBox, Color.Green);
+            //ChangeStatus(LabelBox, Color.Red);
+
             foreach (string file in folders_full)
             {
-                var a = DateTime.Today.AddDays(DayAgo);
+                DirectoryInfo file_1 = new DirectoryInfo(_iikoPosUsersFolder);
+                MessageBox.Show(file_1.LastAccessTime.ToString());
+
 
                 if (DateTime.Today.AddDays(DayAgo) >= Directory.GetLastAccessTime(file))
                 {
@@ -87,49 +89,40 @@ namespace WorkspaceCleaner
                     }
                     catch (Exception ex)
                     {
+                        
 
                         File.AppendAllText("..\\logs\\logs.log", $"{DateTime.Now} {file} INFO [{name}]  - {ex}\n");
                     }
 
 
                 }
-
+                //ChangeStatus(LabelBox, Color.Green);
             }
-            ChangeStatus(pic_box, true);
+            
 
         }
         //Удаление файлов из TEMP
         private void TemporaryFilesDel()
         {
+            Thread.Sleep(10000);
             string DataPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-            DataPath = $"{DataPath}\\AppData\\Local\\Temp";
-            DeleteFiles(label1, FilesNames(DataPath), "TEMP", 1);
-        }
-        //Возвращаем список путей для всех файлов и папок 
-        private IEnumerable<string> FilesNames(string DataPath)
-        {
-            return Directory.EnumerateFileSystemEntries(DataPath);
+            DataPath = $@"{DataPath}\AppData\Local\Temp";
+            DeletedFromInfoFiles(DataPath,"TEMP", label1,1);
         }
 
         //Удаление файлов из ETLLogs
         private void ProgramFilesDel()
         {
-            string DataPath = "C:\\ProgramData\\Microsoft\\Diagnosis\\ETLLogs";
-            DeleteFiles(label1, FilesNames(DataPath), "ETLLogs", 1);
+            string DataPath = @"C:\ProgramData\Microsoft\Diagnosis\ETLLogs";
+            DeletedFromInfoFiles(DataPath, "ETLLogs", label1,1);
         }
         // Удаление логов POS
         private void LogsFilesDel()
         {
             string DataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            DataPath = $"{DataPath}\\iiko\\CashServer\\Logs";
-            DeleteFiles(label2, FilesNames(DataPath), "iikoFrontLogs", -30);
-        }
-
-        //Удаления файлов из загрузки
-        private void LoadingFilesDel()
-        {
-            string DataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
-            DeleteFiles(label5, FilesNames(DataPath), "Loading", 1);
+            DataPath = $@"{DataPath}\iiko\CashServer\Logs";
+            DeletedFromInfoFiles(DataPath, "iikoFrontLogs", label2,-30);
+            
         }
 
         //Определяем путь для POS сервера
@@ -152,11 +145,74 @@ namespace WorkspaceCleaner
             return false;
         }
 
+
         //Удаление данных по RMS
         private void RMSFilesDel()
         {
-            string DataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "iiko\\RMS");
-            DeleteFiles(label4, FilesNames(DataPath), "RMS", 1);
+            string DataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"iiko\RMS");
+            DeletedFromInfoFiles(DataPath, "RMS", label4, 1);
+            //DeleteFiles(label4, Directory.EnumerateFileSystemEntries(DataPath), "RMS", 1);
         }
+        //Удаления файлов из загрузки
+        private void LoadingFilesDel()
+        {
+            string DataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+            DeletedFromInfoFiles(DataPath, "Loading", label5, 1);
+        }
+
+
+        private void DeletedFromInfoFiles(string PathForDel,string NameMethods,Label LabelMethods, sbyte DayAgo)
+        {
+            ChangeStatus(label1, Color.Red);
+
+            var DirectoryForDel = new DirectoryInfo(PathForDel); //Получил информацию об исходной дириктории
+            DirectoryInfo[] Dirs = DirectoryForDel.GetDirectories(); // Какие папки находятся в искомой директории
+            
+
+
+            foreach (DirectoryInfo DirForDel in Dirs) //Удаление папок
+            {
+                if (DateTime.Today.AddDays(DayAgo) >= DirForDel.LastAccessTime)
+
+                { 
+                
+                
+                try
+                {
+                        DirForDel.Delete(true);
+                    }
+                    catch (Exception ex)
+                {
+                    File.AppendAllText("..\\logs\\logs.log", $"{DateTime.Now} {DirForDel.FullName} INFO [{NameMethods}]  - {ex.Message}\n");
+                }
+                }
+
+            }
+
+            FileInfo[] FilesForDel = DirectoryForDel.GetFiles(); //Информация о файлах, которые находятся в исходной папке 
+            foreach (FileInfo FileDeleted in FilesForDel) //Удаление файлов
+            {
+                if (DateTime.Today.AddDays(DayAgo) >= FileDeleted.LastAccessTime)
+                { 
+                    if (DateTime.Today.AddDays(DayAgo) >= FileDeleted.LastAccessTime)
+                    { 
+                        try
+                        {
+                            //Console.WriteLine(FileDeleted.FullName);
+                             FileDeleted.Delete();
+                        }
+                        catch (Exception ex)
+                        {
+                            File.AppendAllText("..\\logs\\logs.log", $"{DateTime.Now} {FileDeleted.FullName} INFO [{NameMethods}]  - {ex.Message}\n");
+                        }
+                    }
+
+                }
+                ChangeStatus(label1, Color.Green);
+
+
+            }
+        }
+
     }
 }
